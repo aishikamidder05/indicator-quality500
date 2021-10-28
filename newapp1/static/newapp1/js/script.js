b432f8e8b2422574ba3532264b548cf68c462a81
Aishika Midder <aishikamidder50@gmail.com>
2021-10-29 00:57:14 +0530

    custom conditions/ progress bar levels

--- newapp1/static/newapp1/js/script.js
+++ newapp1/static/newapp1/js/script.js
@@ -15,17 +15,34 @@ reviewfield.addEventListener("keyup", (e) => {
 
   if (reviewval.length > 0) {
     progressarea.style.display = "block";
-   if(depth>=500){
-    progresstext.innerText='High';
-    progresstext.classList.remove('w-50','bg-danger');
-    progresstext.classList.add('w-100','bg-success');
+   if(depth>=801){
+    progresstext.innerText='Very High';
+    progresstext.classList.remove('verylow', 'low', 'medium', 'high');
+    progresstext.classList.add('veryhigh');
 
    }
-  
-   else{
+   else if(depth>=601 && depth<=800){
+    progresstext.innerText='High';
+    progresstext.classList.remove('verylow', 'low', 'medium', 'veryhigh');
+    progresstext.classList.add('high');
+
+   }
+   else if(depth>=401 && depth<=600){
+    progresstext.innerText='Medium';
+    progresstext.classList.remove('verylow', 'low', 'high', 'veryhigh');
+    progresstext.classList.add('medium');
+
+   }
+   else if(depth>=201 && depth<=400){
     progresstext.innerText='Low';
-    progresstext.classList.remove('w-100','bg-success');
-    progresstext.classList.add('w-50','bg-danger');
+    progresstext.classList.remove('verylow', 'medium', 'high', 'veryhigh');
+    progresstext.classList.add('low');
+
+   }
+   else{
+    progresstext.innerText='Very Low';
+    progresstext.classList.remove('veryhigh', 'low', 'high', 'medium');
+    progresstext.classList.add('verylow');
    }
   }
 }); 
